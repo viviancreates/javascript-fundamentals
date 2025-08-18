69dd1aab6af86ebd377c57d340b33c067d7d51c9
Vivian Davila <viviandavilacodes@gmail.com>
2025-08-18 15:24:57 -0500

    Add notes, intro to props

--- react-playground/hello-react/src/App.jsx
+++ react-playground/hello-react/src/App.jsx
@@ -1,8 +1,20 @@
-function App() {
+import doggo from './assets/corgi-ball.png'
 
+function App() {
+  //This is where we put variables, functions, and other things we need
+  const todaysDate = new Date().toLocaleDateString();
+
+
+  // The return is for rendering the markup
   return (
     //jsx code goes here
+    <>
     <h1>Hello world</h1>
+     {/* We have access to anything in this function called App -> use todaysDate */}
+    <p>Today's date is: {todaysDate}</p>
+    <img src="/cute.jpeg" alt="Cute Image" />
+    <img src={doggo} alt="Corgi Ball" />
+    </>
   )
 }
 
