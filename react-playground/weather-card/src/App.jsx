03a5ef71d4144bf55d70fc1ce20d1e3660189587
Vivian Davila <viviandavilacodes@gmail.com>
2025-08-19 12:54:15 -0500

    Continue notes on react

--- react-playground/weather-card/src/App.jsx
+++ react-playground/weather-card/src/App.jsx
@@ -1,8 +1,17 @@
+import Weather from "./components/weather/Weather";
 
 function App() {
 
   return (
-   x
+    <>
+      <h1>Weather Forecast</h1>
+      <div className="weather-grid">
+        <Weather city="Miami" temperature={85} />
+        <Weather city="London" temperature={58} condition="Cloudy" />
+        <Weather city="Seattle" temperature={63} condition="Rainy" />
+        <Weather city="Denver" temperature={45} condition="Stormy" />
+      </div>
+    </>
   );
 };
 
