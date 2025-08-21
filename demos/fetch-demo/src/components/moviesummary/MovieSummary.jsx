55abce458018b41c11557e427cb233020c5368af
Vivian Davila <viviandavilacodes@gmail.com>
2025-08-21 15:06:14 -0500

    Add cors configuration for backend and start frontend demo

--- demos/fetch-demo/src/components/moviesummary/MovieSummary.jsx
+++ demos/fetch-demo/src/components/moviesummary/MovieSummary.jsx
@@ -10,9 +10,20 @@ function MovieSummary() {
     useEffect(() => {
         const fetchMovieSummaries = async () => {
             try {
+                // mark the component as loading
                 setLoading(true);
+
+                //call the api
+                const response = await fetch(`http://localhost:8080/api/movies/summaries`);
+
+                //check for a bad response
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
                 const data = await getMovieSummaries();
                 setMovies(data);
+                
             } catch (err) {
                 setError(err);
             } finally {
